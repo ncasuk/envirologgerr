7ce18298d9800895e59c7a628f8b62b68e67b285
Dan Walker <daniel.walker@ncas.ac.uk>
2019-07-11 14:55:28 +0100

    must be a character vector. Refs #2920

--- R/get_envirologger_data.R
+++ R/get_envirologger_data.R
@@ -246,7 +246,7 @@ get_envirologger_data_worker <- function(url, tz, user, key, verbose) {
       
     })
     #DEBUG
-    print(response$TBTimestamp, response$TETimestamp)
+    print(c(response$TBTimestamp, response$TETimestamp))
     
     # Another catch
     if (!is.null(response)) {
